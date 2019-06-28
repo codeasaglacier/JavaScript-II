9b847289514aa446e893741ab6ed12c0f5b4c741
codeasaglacier <remingtonshock@gmail.com>
2019-06-28 15:35:23 -0800

    finished most of callbacks, getting help on last problem

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,73 +1,128 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
   //Given this problem: 
   
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
-
-  // Potential Solution:
-
-  // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
-
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
-
-*/
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
+  // // Potential Solution:
+  // // Higher order function using "cb" as the call back
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
+  // // Function invocation 
+  // firstItem(items, function(first) {
+  //   console.log(first)
+  // });
 
 
-// function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-// }
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-const getLength=(arr,cb)=>cb(arr.length);
-getLength(items,length=>{
-  console.log(`The length of the array is ${length}.`)
-});
+function getLength(arr, cb) {
+//   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
+}
+getLength(items, function(length){
+  console.log(length)
+})
 
-// function last(arr, cb) {
+ function last(arr, cb) {
 //   // last passes the last item of the array into the callback.
-// }
+  return cb(arr[arr.length-1])
+}
+last(items, function(end){
+  console.log(end)
+})
 
-const getLast=(array,cb)=>cb(array[array.length-1]);
-getLast(items,length=>{
-  console.log(`The last item of the array is ${length}`)});
-
-// function sumNums(x, y, cb) {
+function sumNums(x, y, cb) {
 //   // sumNums adds two numbers (x, y) and passes the result to the callback.
-// }
-const sumNums=(x,y,cb)=>
-    cb(x+y);
-sumNums(1, 2, (sumOf) =>
-  console.log(sumOf))
+  return cb(x + y)
+}
+sumNums(4,5, function(total){
+  console.log(total)
+})
+
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+//   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y)
 }
+multiplyNums(2,3, function(product){
+  console.log(product)
+})
 
-// function contains(item, list, cb) {
+function contains(item, list, cb) {
 //   // contains checks if an item is present inside of the given array/list.
 //   // Pass true to the callback if it is, otherwise pass false.
-// }
-// const contains=(items,list,cb)=>
-//     if(a)
-
-
-
-/* STRETCH PROBLEM */
-
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+if(list.includes(item)){
+  return (true)
+} else {
+  return(false)
 }
+}
+contains(3, [1,2,3], function(present){
+  console.log(present)
+})
+
+// /* STRETCH PROBLEM */
+
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// const getLength=(arr,cb)=>cb(arr.length);
+// getLength(items,length=>{
+//   console.log(`The length of the array is ${length}.`)
+// });
+
+
+// const getLast=(array,cb)=>cb(array[array.length-1]);
+// getLast(items,length=>{
+//   console.log(`The last item of the array is ${length}`)});
+
+
+// const sumNums=(x,y,cb)=>
+//     cb(x+y);
+// sumNums(1, 2, (sumOf) =>
+//   console.log(sumOf))
+
+
